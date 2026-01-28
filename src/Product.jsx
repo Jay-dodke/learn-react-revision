baa24d40226aa294d34463cade723b43d691ae71
Jay Dodke <217025570+Jay-dodke@users.noreply.github.com>
2026-01-28 18:17:19 +0530

    complete Array

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,9 +1,15 @@
 import "./Product.css";
-function Product({title, Price}) {
+function Product({title, Price, Features}) {
   return (
     <div className="ProductCompont">
       <h1>{title}</h1>
       <h5>Price: {Price}</h5>
+      <p>
+        Features:{" "}
+        {Features.map((feature) => (
+          <li>{feature}</li>
+        ))}
+      </p>
     </div>
   );
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,10 +1,11 @@
 import Product from "./Product.jsx";
 function ProductTab() {
+  let Features = ["Good Camera", "Fast Processor", "Long Battery Life"];
   return (
     <>
-      <Product title="Phone" Price={90000} />
-      <Product title="Tablet" Price={50000} />
-      <Product title="Laptop" Price={150000} />
+      <Product title="Phone" Price={90000} Features={Features} />
+      <Product title="Tablet" Price={50000} Features={Features} />
+      <Product title="Laptop" Price={150000} Features={Features} />
     </>
   );
 }
